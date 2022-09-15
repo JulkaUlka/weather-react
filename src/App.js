4f2fadbb4d44d33e31dc347ce3aa60cc55285d09
JulkaUlka <y.glushko@outlook.com>
2022-09-15 18:52:52 +0300

    created main look of the app

--- src/App.js
+++ src/App.js
@@ -3,52 +3,63 @@ import axios from "axios";
 import React, { useState } from "react";
 
 export default function App() {
-  let [city, setCity] = useState(" ");
-  let [message, setMessage] = useState(" ");
-  let apiKey = "ff425fafca53dfa00a8edad5ed4545b5";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  let [temperature, setTemperature] = useState(null);
-  let [description, setDescription] = useState(null);
-  let [humidity, setHumidity] = useState(null);
-  let [wind, setWind] = useState(null);
-  let [icon, setIcon] = useState(null);
-
-  axios.get(apiUrl).then(showWeather);
-  function showWeather(response) {
-    setTemperature(response.data.main.temp);
-    setDescription(response.data.weather[0].description);
-    setHumidity(response.data.main.humidity);
-    setWind(response.data.wind.speed);
-    setIcon(
-      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-    );
-  }
-
-  function handlSubmit(event) {
-    event.preventDefault();
-    setMessage(
-      <ul>
-        <li>temperature: {Math.round(temperature)}°C </li>
-        <li>description: {description}</li>
-        <li>wind speed: {wind} km/h</li>
-        <li>humidity: {humidity} %</li>
-        <li>
-          {" "}
-          <img src={icon} alt={description} />
-        </li>
-      </ul>
-    );
-  }
-  function updateCity(event) {
-    setCity(event.target.value);
-  }
+  let weatherData = {
+    city: "Kyiv",
+    temperature: 19,
+    day: "Tuesday 1",
+  };
   return (
-    <div className="forecast">
-      <form onSubmit={handlSubmit}>
-        <input type="search" onChange={updateCity} />
-        <input type="submit" value="Search" />
-      </form>
-      <p>{message}</p>
+    <div>
+      <div className="Weather">
+        <div className="row">
+          <div className="col-4">
+            <h1> {weatherData.city} </h1>
+
+            <div> {weatherData.day}</div>
+          </div>
+
+          <div className="col-4">
+            <span className="current-temperature">
+              {weatherData.temperature}
+            </span>
+            <a href="/" className="current-temperature measures ">
+              {" "}
+              C{" "}
+            </a>
+
+            <a href="/" className="current-temperature measures ">
+              {" "}
+              F{" "}
+            </a>
+          </div>
+          <div className="col-3">
+            <form>
+              <input
+                type="search"
+                name="text"
+                className="search"
+                placeholder="Search city"
+                autoComplete="off"
+              />
+            </form>
+          </div>
+          <div className="col-1">
+            <button className="currentLocation">
+              <span>📍</span>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="signature">
+        {" "}
+        <p>
+          Open source code by{" "}
+          <a href="https://github.com/JulkaUlka/weather-react" target="_blank">
+            {" "}
+            Yuliia Hlushko{" "}
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
